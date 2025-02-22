ba218bbd9fa4a6429ab3ac8b930eddf45e89935d
lcn <836886548@qq.com>
2025-02-23 01:36:33 +0800

    移除 ElementPlus 依赖；添加客户端构建脚本和自动导入、组件插件支持

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,6 @@
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
-
-app.use(ElementPlus)
 
 app.mount('#app')
