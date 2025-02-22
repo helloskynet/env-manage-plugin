3806dc0da6517b24ad300b9325723e40f4ee082b
lcn <836886548@qq.com>
2025-02-22 23:59:14 +0800

    优化服务器配置，使用环境变量设置管理端口、开发服务器URL和基础路径

--- src/index.js
+++ src/index.js
@@ -19,24 +19,23 @@ const envConfigPath = path.resolve(
 );
 
 const envConfig = require(envConfigPath);
+const managePort = envConfig.port || 3099;
+const devServerUrl = envConfig.devServerUrl || "http://localhost:5173";
+const basePath = envConfig.basePath || "/dev-manage-api";
 
 const ManageServer = require("./ManageServer");
-
 ManageServer.envList = envConfig.envList;
 
 // 使用前置转发  所有请求都会先转发到webpack-dev-server
 const PreProxyServer = require("./PreProxyServer");
-const preProxyServer = new PreProxyServer(
-  envConfig.devServerUrl || "http://localhost:5173"
-);
+const preProxyServer = new PreProxyServer(devServerUrl);
 
 // 后置转发 和 管理路由
 const PostProxyServer = require("./PostProxyServer");
-const { env } = require("process");
-const postProxyServer = new PostProxyServer(envConfig.port || 3099);
+const postProxyServer = new PostProxyServer(managePort);
 
 const manageServer = new ManageServer(
   preProxyServer.getApp,
   postProxyServer.getApp,
-  envConfig.basePath || "/dev-manage-api"
+  basePath
 );
