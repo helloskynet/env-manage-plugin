ea5fdc8a2a4441e46d3455139cb5f5c4829a8fd7
lcn <836886548@qq.com>
2025-03-02 16:15:51 +0800

    使用ts实现

---
+++ srcts/PostProxyServer.ts
@@ -0,0 +1,125 @@
+const path = require("path");
+const express = require("express");
+const expressStaticGzip = require("express-static-gzip");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const Utils = require("./Utils");
+const ManageRouter = require("./ManageRouter");
+/**
+ * 后置代理服务器---同时也是管理页面的服务器
+ */
+class PostProxyServer {
+  get envConfig() {
+    return this.manageRouter.envConfig;
+  }
+
+  set envConfig(newConfig) {
+    const newEnvList = this.udpateEnvList(newConfig);
+    const envConfig = {
+      ...newConfig,
+      envList: newEnvList,
+    };
+    this.manageRouter.envConfig = envConfig;
+
+    this.preProxyServer.envConfig = envConfig;
+  }
+
+  constructor(envConfig, preProxyServer) {
+    // this.envConfig = envConfig;
+    this.preProxyServer = preProxyServer;
+
+    // 初始化服务器
+    this.app = this.initializeServer();
+
+    // 初始化管理路由
+    this.manageRouter = new ManageRouter(preProxyServer, envConfig);
+    this.app.use(envConfig.basePath, this.manageRouter.getRouter());
+
+    // 启动服务器
+    this.startServer();
+  }
+
+  initializeServer() {
+    const app = express();
+
+    // 代理中间件
+    app.use(this.createPostProxyMiddleware());
+
+    // 无需代理的数据继续下一个中间件
+    app.use(this.errorHandler);
+
+    // 静态资源
+    app.use(expressStaticGzip(path.join(__dirname, "client")));
+
+    return app;
+  }
+
+  createPostProxyMiddleware() {
+    return createProxyMiddleware({
+      changeOrigin: true,
+      ws: true,
+      router: (req) => {
+        if (req.headers["x-api-server"]) {
+          const port = req.headers["x-api-server"];
+
+          const env = this.preProxyServer.appMap[port].x_env;
+
+          if (env?.router) return env.router(req, env);
+          if (env?.target) return env.target;
+        }
+        throw new Error("SKIP_PROXY");
+      },
+    });
+  }
+
+  errorHandler(err, req, res, next) {
+    if (err.message === "SKIP_PROXY") return next();
+    res.status(500).send("代理服务器出错");
+  }
+
+  udpateEnvList(newConfig) {
+    const { envList: newEnvList, indexPage, devServerList } = newConfig;
+    const { envList: oldEnvList = [] } = this.envConfig;
+
+    const newListAfter = Utils.removeEnvDuplicates(newEnvList);
+
+    const oldEnvMap = Utils.generateMap(oldEnvList); // 生成端口号到环境配置的映射
+
+    const devServerMap = Utils.generateMap(devServerList, ["name"]);
+
+    const defautlDevserverName = devServerList[0]?.name ?? "";
+
+    // 返回新的环境配置
+    const newList = newListAfter.map((item) => {
+      const rowKey = `${item.name}+${item.port}`;
+
+      let devServerName =
+        oldEnvMap?.[rowKey]?.devServerName || item.devServerName;
+
+      if (!devServerMap[devServerName]) {
+        devServerName = defautlDevserverName;
+      }
+
+      return {
+        ...item,
+        indexPage: `${item.indexPage || indexPage || ""}`,
+        devServerName,
+      };
+    });
+
+    return newList;
+  }
+
+  /**
+   * 启动服务
+   */
+  startServer() {
+    this.app.listen(this.envConfig.port, () => {
+      console.log(
+        `Post Proxy Middleware is running on http://localhost:${this.envConfig.port}`
+      );
+    });
+  }
+}
+
+module.exports = PostProxyServer;
---
+++ srcts/index.ts
@@ -0,0 +1,151 @@
+const fs = require("fs");
+const path = require("path");
+const chokidar = require("chokidar");
+const { pathToFileURL } = require("url");
+
+const Utils = require("./Utils");
+const PreProxyServer = require("./PreProxyServer");
+const PostProxyServer = require("./PostProxyServer");
+
+class EnvManage {
+  configFileCacheBuster = 0; // 缓存破坏者
+
+  constructor(options = {}) {
+    this.options = options;
+
+    const configPath = this.options?.config || "./env.config.js";
+    this.configPath = path.resolve(process.cwd(), configPath);
+
+    this.manageServer = {};
+  }
+
+  apply(compiler) {
+    compiler.hooks.afterPlugins.tap("EnvManagePlugin", (compilation) => {
+      this.startIndependent();
+    });
+  }
+
+  async getEnvPluginConfig() {
+    const modulePath = this.configPath;
+
+    let envConfig = {};
+    try {
+      // 清除缓存（仅对 CommonJS 有效）
+      delete require.cache[require.resolve(modulePath)];
+
+      // 判断文件类型
+      if (
+        modulePath.endsWith(".mjs") ||
+        (modulePath.endsWith(".js") && Utils.isESModule(modulePath))
+      ) {
+        // 动态加载 ES Module
+
+        const fileUrl = pathToFileURL(path.resolve(modulePath)).href;
+
+        const urlWithCacheBuster = `${fileUrl}?v=${this.configFileCacheBuster++}`;
+        const module = await import(urlWithCacheBuster);
+        envConfig = module.default || module;
+      } else {
+        // 使用 require 加载 CommonJS
+        envConfig = require(modulePath);
+      }
+    } catch (error) {
+      console.error(`Failed to load module at ${modulePath}:`, error);
+    }
+
+    const {
+      port = 3099,
+      basePath = "/dev-manage-api",
+      envList = [],
+      devServerList = [],
+      indexPage = "",
+    } = envConfig;
+
+    this.envConfig = {
+      port,
+      basePath,
+      envList,
+      devServerList,
+      indexPage,
+    };
+    console.log("Config file loaded");
+  }
+
+  async startIndependent() {
+    await this.getEnvPluginConfig();
+
+    this.preProxyServer = new PreProxyServer(this.envConfig);
+
+    this.manageServer = new PostProxyServer(
+      this.envConfig,
+      this.preProxyServer
+    );
+
+    this.updatePostProxyServerConfig();
+
+    this.watchConfig();
+  }
+
+  updatePostProxyServerConfig() {
+    this.manageServer.envConfig = this.envConfig;
+  }
+
+  watchConfig() {
+    const watcher = chokidar.watch(this.configPath, {
+      persistent: true,
+    });
+
+    let debounceTimer;
+    watcher.on("change", () => {
+      console.log("Config file changed");
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(() => {
+        this.getEnvPluginConfig()
+          .then(() => {
+            this.updatePostProxyServerConfig();
+          })
+          .catch((error) => {
+            console.error("Error updating config:", error);
+          });
+      }, 500);
+    });
+  }
+
+  /**
+   * 初始化配置文件
+   * @param {boolean} force - 是否强制覆盖
+   */
+  static initConfig(force = false) {
+    const FILE_EXT = Utils.isESModuleByPackageJson() ? ".mjs" : ".js";
+
+    const CONFIG_FILE_NAME = `env.config${FILE_EXT}`;
+    // 模板文件路径
+    const TEMPLATE_PATH = path.join(
+      __dirname,
+      `../templates/${CONFIG_FILE_NAME}`
+    );
+    // 目标文件路径
+    const TARGET_PATH = path.join(process.cwd(), CONFIG_FILE_NAME);
+    try {
+      // 检查目标文件是否存在
+      const isTargetExist = fs.existsSync(TARGET_PATH);
+
+      if (isTargetExist && !force) {
+        console.log(
+          `${CONFIG_FILE_NAME} already exists. Use --force to overwrite.`
+        );
+        return;
+      }
+
+      // 拷贝模板文件到目标路径
+      fs.copyFileSync(TEMPLATE_PATH, TARGET_PATH);
+      console.log(
+        `${CONFIG_FILE_NAME} ${isTargetExist ? "overwritten" : "created"} successfully!`
+      );
+    } catch (err) {
+      console.error("Failed to initialize env.config.js:", err);
+    }
+  }
+}
+
+module.exports = EnvManage;
