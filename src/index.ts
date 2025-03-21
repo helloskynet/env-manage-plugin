cefbb3080ee8c09895bdcaf281053a25aed90ca3
lcn <836886548@qq.com>
2025-03-21 20:23:42 +0800

    重构 EnvManage 类，简化配置加载逻辑并移除冗余代码

--- src/index.ts
+++ src/index.ts
@@ -8,47 +8,23 @@ class EnvManage {
    */
   options: Options;
 
-  /**
-   * 配置文件地址
-   */
-  configPath: string;
-
-  /**
-   * 后置代理服务器 和 管理服务器
-   */
-  manageServer: PostProxyServer | null = null;
-
-  /**
-   * 配置信息
-   */
-  config: Config;
-
   constructor(options: Options = {}) {
     this.options = options;
   }
 
-  /**
-   * 加载插件配置
-   * @returns
-   */
-  getEnvPluginConfig() {
-    return this.config.initConfig(this.options.config).then(() => {
-      console.log("Config file loaded");
-    });
-  }
-
   async startIndependent(isPlugin: boolean = false) {
-    this.config = new Config(isPlugin);
+    const config = new Config(isPlugin);
 
-    this.getEnvPluginConfig()
+    await config
+      .initConfig(this.options.config)
       .then(() => {
-        this.manageServer = new PostProxyServer();
+        console.log("Config file loaded");
+        new PostProxyServer();
       })
       .catch(() => {
         // todo 错误处理
       });
   }
-
 }
 
 export { EnvManage };
